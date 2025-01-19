ed62391dc188fee753bce858c78b6061db08ae9f
rainkyzhong <rainkyzhong@sina.com>
2025-01-20 00:37:12 +0800

    feat: 按钮测试文件

--- dev/src/test/button.tsx
+++ dev/src/test/button.tsx
@@ -13,27 +13,20 @@ export const ButtonTest = function () {
       setLoading(false)
     }, 4000);
   }
+  const elements = []
+  for (const variant of ['base', 'outline', 'dashed', 'text']) {
+    for (const type of ['default', 'primary', 'danger', 'safe', 'warn']) {
+      elements.push(
+        // @ts-nocheck
+        // @ts-ignore
+        <FButton loading={isLoading} onClick={onClick} variant={variant} type={type}>提交</FButton>
+      )
+    }
+  }
   return (<>
     <h1>按钮组件测试</h1>
-    <FButton loading={isLoading} onClick={onClick}>下载安装包</FButton>
-    <FButton loading={isLoading} onClick={onClick} type="primary"></FButton>
-    <FButton loading={isLoading} onClick={onClick} type="danger"></FButton>
-    <FButton loading={isLoading} onClick={onClick} type="safe"></FButton>
-    <FButton loading={isLoading} onClick={onClick} type="warn"></FButton>
-    <FButton loading={isLoading} variant="outline" onClick={onClick}></FButton>
-    <FButton loading={isLoading} variant="outline" onClick={onClick} type="primary"></FButton>
-    <FButton loading={isLoading} variant="outline" onClick={onClick} type="danger"></FButton>
-    <FButton loading={isLoading} variant="outline" onClick={onClick} type="safe"></FButton>
-    <FButton loading={isLoading} variant="outline" onClick={onClick} type="warn"></FButton>
-    <FButton loading={isLoading} variant="dashed" onClick={onClick}></FButton>
-    <FButton loading={isLoading} variant="dashed" onClick={onClick} type="primary"></FButton>
-    <FButton loading={isLoading} variant="dashed" onClick={onClick} type="danger"></FButton>
-    <FButton loading={isLoading} variant="dashed" onClick={onClick} type="safe"></FButton>
-    <FButton loading={isLoading} variant="dashed" onClick={onClick} type="warn"></FButton>
-    <FButton loading={isLoading} variant="text" onClick={onClick}></FButton>
-    <FButton loading={isLoading} variant="text" onClick={onClick} type="primary"></FButton>
-    <FButton loading={isLoading} variant="text" onClick={onClick} type="danger"></FButton>
-    <FButton loading={isLoading} variant="text" onClick={onClick} type="safe"></FButton>
-    <FButton loading={isLoading} variant="text" onClick={onClick} type="warn"></FButton>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 10 }}>
+      {elements}
+    </div>
   </>)
 }
