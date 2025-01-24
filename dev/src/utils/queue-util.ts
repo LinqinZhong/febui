b268f41c21b27c081f4474659a6e9eedc6ded5f6
钟林钦 <linqin.zhong@rastar.gd>
2025-01-24 18:03:28 +0800

    refactor: 重构任务队列

--- dev/src/utils/queue-util.ts
+++ dev/src/utils/queue-util.ts
@@ -4,57 +4,55 @@
  * @date 2025/01/24 17:43
  * @example
  * const current = Date.now()
- * createTaskQueue().enqueue((pre,next) => {
- *      setTimeout(() => {
- *          console.log('a', Date.now() - current)
- *          next('hello')
- *      },1000)
- *  }).enqueue((pre,next) => {
- *      setTimeout(() => {
- *          console.log('b', Date.now() - current)
- *          console.log(pre)
- *          next(pre)
- *      },1000)
- *  })
- *  // > a 1000
- *  // > b 2000
- *  // > hello
+ * createTaskQueue().enqueue((pre, next) => {
+ *     setTimeout(() => {
+ *         console.log('a', Math.round((Date.now() - current)/1000))
+ *         next('hello')
+ *     }, 1000)
+ * }).enqueue((pre, next) => {
+ *     setTimeout(() => {
+ *         console.log('b',Math.round((Date.now() - current)/1000))
+ *         console.log(pre)
+ *         next(pre)
+ *     }, 1000)
+ * })
+ * // > a 1
+ * // > b 2
+ * // > hello
  */
 export const createTaskQueue = () => {
+    // 排队期约
+    let p: Promise<any> | null
     // 生成队列
     const queue: {
-        // 排队期约
-        p: Promise<any> | null,
         // 队中任务个数
-        n: number
-    } = {
-        p: null,
-        n: 0
-    }
-    return {
+        n: Readonly<number>
         /**
          * 入队
          * @param handler - 排队函数
          */
-        enqueue(
+        enqueue: (
             handler: (
                 // 增量信息
                 preInfo: any,
                 // 放行函数
                 next: (preInfo: any) => void
             ) => void
-        ) {
-            if (!queue.p) {
-                queue.p = new Promise((next) => {
+        ) => typeof queue
+    } = {
+        n: 0,
+        enqueue(handler) {
+            if (!p) {
+                p = new Promise((next) => {
                     handler(null, next)
                 })
 
             } else {
                 queue.n++
-                queue.p = queue.p.then((preInfo) => {
+                p = p.then((preInfo) => {
                     queue.n--
                     // 队列无任务时清除记录的最后一个期约
-                    if (queue.n === 0) queue.p = null
+                    if (queue.n === 0) p = null
                     return new Promise((next) => {
                         handler(preInfo, next)
                     })
@@ -63,4 +61,5 @@ export const createTaskQueue = () => {
             return queue
         }
     }
+    return queue
 }
