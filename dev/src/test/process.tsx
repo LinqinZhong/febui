cfe891d692467747230cd3a8c607efba6b524e7f
rainkyzhong <rainkyzhong@sina.com>
2025-02-01 19:18:35 +0800

    feat: 侧边栏支持滚动

---
+++ dev/src/test/process.tsx
@@ -0,0 +1,10 @@
+/**
+ * @description 进度条（FProcess）
+ * @file process.tsx
+ * @author linqin.zhong
+ * @date 2025/02/01 16:17:53
+*/
+import React, { useState } from "react"
+export const FProcessTest = function () {
+  return (<>FProcess</>)
+}
---
+++ dev/src/test/tab.tsx
@@ -0,0 +1,10 @@
+/**
+ * @description 选项卡（FTab）
+ * @file tab.tsx
+ * @author linqin.zhong
+ * @date 2025/02/01 16:16:22
+*/
+import React, { useState } from "react"
+export const FTabTest = function () {
+      return (<>FTab</>)
+}
