e4fa8793f3cd64d3afab8e768e6266815bd87ec3
tong <2818411581@qq.com>
2025-02-05 17:45:29 +0800

    feat: switch组件

---
+++ dev/src/component/form/switch/FSwitch.tsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import styles from './style.module.less'
+import { Size } from '@/enum/base'
+import { isBoolean } from '@/utils/type.util'
+import { FLoading } from '#/base/loading/FLoading'
+
+
+// 组件属性
+type Props = FebProps<{
+  value: boolean | string | number
+  disabled?: boolean
+  size?: ButtonSize
+  width?: number
+  height?: number
+  activeText?: string
+  inactiveText?: string
+  inline?: boolean
+  activeValue?: boolean | string | number
+  inactiveValue?: boolean | string | number
+  loading?: boolean
+
+  onChange?: (v: boolean | string | number, context: { e: React.SyntheticEvent }) => void
+  beforeChange?: boolean | beforeChangeFn,
+}>
+
+export default class FSwitch extends Component<Props> {
+
+  state: Readonly<{ isActive: boolean }> = { isActive: true }
+
+  static defaultProps = {
+    size: Size.Medium,
+    disabled: false,
+    inline: false,
+    value: true,
+    activeValue: true,
+    inactiveValue: false,
+    loading: false
+  }
+
+  async handleChange(e: React.SyntheticEvent) {
+    if (this.props.onChange && !this.props.disabled) {
+      try {
+        let flag = true
+        if (this.props.beforeChange !== undefined) {
+          if (isBoolean(this.props.beforeChange)) {
+            flag = this.props.beforeChange
+          } else {
+            flag = await this.props.beforeChange()
+          }
+        }
+        flag && this.state.isActive ? this.props.onChange(this.props.inactiveValue!, { e }) : this.props.onChange(this.props.activeValue!, { e })
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  changeActiveStatus(value: typeof this.props.value) {
+    if (value === this.props.activeValue || value === this.props.inactiveValue) {
+      value === this.props.activeValue ? this.setState({ isActive: true }) : this.setState({ isActive: false })
+    } else {
+      throw new Error('value值和 active-value 、 inactive-value 无法匹配')
+    }
+  }
+
+  // 监听value的变化
+  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<{}>, snapshot?: any): void {
+    if (prevProps.value !== this.props.value) {
+      this.changeActiveStatus(this.props.value)
+    }
+  }
+
+  render() {
+
+    const { isActive } = this.state
+
+    const { style, width, size, height, activeText, inactiveText, inline, disabled, loading } = this.props
+
+    const switchClass = [
+      styles.switch,
+      disabled && styles['disabled']
+    ]
+    const coreClass = [
+      styles.core,
+      styles[size ? size : Size.Medium as string],
+      width && styles['nofit'],
+      styles[isActive ? 'on' : 'off']
+    ]
+    const actionClass = [
+      styles['action'],
+      styles[isActive ? 'left' : 'right']
+    ]
+
+    const switchStyles = Object.assign(
+      {},
+      style,
+      width ? { '--w': width + 'px' } : {},
+      height ? { '--h': height + 'px' } : {}
+    )
+
+    return (
+      <div className={switchClass.join(' ')}>
+        {activeText && !inline && <span className={[styles['inactive-text'], !isActive ? styles['active'] : ''].join(' ')}>{activeText}</span>}
+        <div onClick={(e) => this.handleChange(e)} className={coreClass.join(' ')} style={switchStyles}>
+          {activeText && inline && isActive && <span className={styles['inline-active']}>{activeText}</span>}
+          <div className={actionClass.join(' ')}>
+            {loading && <FLoading className={styles['loading']}></FLoading>}
+          </div>
+          {inactiveText && inline && !isActive && <span className={styles['inline-inactive']}>{inactiveText}</span>}
+        </div>
+        {inactiveText && !inline && <span className={[styles['active-text'], isActive ? styles['active'] : ''].join(' ')}>{inactiveText}</span>}
+      </div>
+    )
+  }
+}
---
+++ dev/types/switch.d.ts
@@ -0,0 +1,1 @@
+type beforeChangeFn = () => Promise<boolean>;
